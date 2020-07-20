4b150d22e6cec641820530354e65ffa1d393609a
Felipe Weiduschadt de Carvalho <felipefln@MacBook-Pro-de-Felipe.local>
2020-07-20 17:18:26 -0300

    Feat: Criado estrutura de toast para apresentação de erro, warning, success.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from "./styles/global";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 
+import ToastContainer from "./components/ToastContainer";
 import { AuthProvider } from "./hooks/AuthContext";
 
 const App: React.FC = () => (
@@ -11,6 +12,8 @@ const App: React.FC = () => (
         <AuthProvider>
             <SignIn />
         </AuthProvider>
+
+        <ToastContainer />
         <GlobalStyle />
     </React.Fragment>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -43,10 +43,12 @@ const SignIn: React.FC = () => {
                     password: data.password,
                 });
             } catch (err) {
-                console.log(err);
-                const errors = getValidationErrors(err);
+                if (err instanceof Yup.ValidationError) {
+                    console.log(err);
+                    const errors = getValidationErrors(err);
 
-                formRef.current?.setErrors(errors);
+                    formRef.current?.setErrors(errors);
+                }
             }
         },
         [signIn]
